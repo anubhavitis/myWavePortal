0cfcb2b53dea0a91a9255079532805bde880374c
anubhav.s <anubhav.s@coinswitch.co>
2022-10-04 04:48:43 +0530

    updated UI

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,9 @@ const getEthereumObject = () => window.ethereum;
  */
 const findMetaMaskAccount = async () => {
   try {
+
+    // TODO: Add an option to make automatic jump to Goreli Test Network
+
     const ethereum = getEthereumObject();
     if (!ethereum) {
       console.error("Make sure you have Metamask!");
@@ -55,6 +58,8 @@ const App = () => {
 
       console.log("Connected", account[0]);
       setCurrentAccount(account[0]);
+
+      location.reload();
     } catch (error) {
       console.error(error);
     }
@@ -71,14 +76,14 @@ const App = () => {
 
         let count = await wavePortalContract.getTotalWaves();
         console.log("Retrieved total wave count...", count.toNumber());
-
+        
         const waveTxn = await wavePortalContract.wave(message);
         console.log("Mining...", waveTxn.hash);
-
+        
         await waveTxn.wait();
         console.log("Mined -- ", waveTxn.hash);
         getAllWaves()
-
+        
         count = await wavePortalContract.getTotalWaves();
         console.log("Retrieved total wave count...", count.toNumber());
       } else {
@@ -88,97 +93,95 @@ const App = () => {
       console.log(error);
     }
   }
-
+  
   const getAllWaves = async () => {
-      try {
-        const { ethereum } = window;
-        if (ethereum) {
-          const provider = new ethers.providers.Web3Provider(ethereum);
-          const signer = provider.getSigner();
-          const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
-  
-          /*
-           * Call the getAllWaves method from your Smart Contract
-           */
-          const waves = await wavePortalContract.getAllWaves();
-
-          let wavesCleaned = [];
-          waves.forEach(wave => {
-            let temp={
-              address: wave.waver,
-              timestamp: new Date(wave.timestamp * 1000),
-              message: wave.message
-            }
-            wavesCleaned.push(temp);
-          });
-  
-          /*
-           * Store our data in React State
-           */
-          setAllWaves(wavesCleaned);
-        } else {
-          console.log("Ethereum object doesn't exist!")
-        }
-      } catch (error) {
-        console.log(error);
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+        const waves = await wavePortalContract.getAllWaves();
+        
+        let wavesCleaned = [];
+        waves.forEach(wave => {
+          let temp = {
+            address: wave.waver,
+            timestamp: new Date(wave.timestamp * 1000),
+            message: wave.message
+          }
+          wavesCleaned.push(temp);
+        });
+        console.log("All waves:", wavesCleaned)
+        setAllWaves(wavesCleaned);
+      } else {
+        console.log("Ethereum object doesn't exist!")
       }
+    } catch (error) {
+      console.log(error);
     }
-  
+  }
+
   useEffect(async () => {
     const account = await findMetaMaskAccount();
     if (account !== null) {
       setCurrentAccount(account);
-      getAllWaves();
     }
-    console.log("Here is list of my waves:", allWaves)
+    getAllWaves();
   }, []);
 
-  // useEffect( () => {
-    
-  // }, [allWaves] )
-  
   return (
-    <div className="mainContainer">
-      <div className="dataContainer">
-        <div className="header">
-          👋 Hey there!
-        </div>
+    <div className="m-4 flex justify-center">
+      <div className="w-full max-w-fit space-y-4 ">
+        <div className=" p-4 m-2 rounded-lg">
+          <h1 className="text-center font-bold">👋 Hey there!</h1>
+          <p className="text-center"> I am Anubhav, full time backend dev at CoinSwitch Kuber.</p>
+          <p className="text-center"> This is my first web3 project, say Hi to me.</p>
 
-        <div className="bio">
-          I am Anubhav and I am new to web3, pretty cool right? Connect your Ethereum wallet and wave at me!
-        </div>
+          {currentAccount ?
+            (
+              <div className="flex justify-center">
+                <input
+                  className="border-2 w-1/2 border-grey p-2 m-2 drop-shadow-lg"
+                  type='text'
+                  placeholder="Type your wave message"
+                  onChange={(e) => setMessage(e.target.value)} />
+                <button
+                  className="border-2 p-2 m-2 bg-lime-400 
+                    border-lime-600 hover:bg-lime-600  drop-shadow-lg
+                    hover:drop-shadow-2xl hover:text-white"
+                  onClick={wave}>
+                  Wave at Me
+                </button>
 
-        {currentAccount? 
-        (
-          <div>
-            <div>
-            <button className="waveButton" onClick={wave}>
-          Wave at Me
-        </button>
               </div>
-            <div>
-        <input 
-          id="myinput"
-          type='text' 
-          placeholder="Type your wave message" 
-          onChange={(e) => setMessage(e.target.value)}/>
+            )
+            :
+            (
+              <div className="text-center">
+                <button
+                  className="border-2 p-2 m-2 bg-lime-400 
+                border-lime-600 hover:bg-lime-600 drop-shadow-lg
+                hover:drop-shadow-2xl hover:text-white"
+                  onClick={connectWallet}>
+                  Connect Wallet
+                </button>
               </div>
-            </div>
-        )
-          : 
-        (<button className="waveButton" onClick={connectWallet}>
-            Connect Wallet
-          </button> )
-        }
-
-        {allWaves.map((wave, index) => {
-          return (
-            <div key={index} style={{ backgroundColor: "lightgreen", marginTop: "16px", padding: "8px" }}>
-              <div>Address: {wave.address}</div>
-              <div>Time: {wave.timestamp.toString()}</div>
-              <div>Message: {wave.message}</div>
-            </div>)
-        })}
+            )
+          }
+        </div>
+        <div className="grid grid-cols-2">
+          {allWaves.slice(0).reverse().map((wave, index) => {
+            return (
+              <div key={index} className="border-2 rounded-t-lg p-2 m-2 hover:bg-gray-100 hover:drop-shadow-lg">
+                <h1 className="p-2 text-center font-medium">👋 {wave.message}</h1>
+                <div className="flex justify-between">
+                  <div className='p-2 w-1/3 truncate'> 👤 {wave.address}</div>
+                  <div className='p-2 w-1/3 truncate text-right'>🗓 {wave.timestamp.toLocaleString()}</div>
+                </div>
+              </div>)
+          })}
+        </div>
       </div>
     </div>
   );
